92c721f1d9b8c2e914d773bf29ea17968101234b
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-04-25 13:38:09 +0600

    added many update of feature section

--- src/views/components/HomeComponents/HomeFeatureSection/HomeFeatureSection.js
+++ src/views/components/HomeComponents/HomeFeatureSection/HomeFeatureSection.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import FeatureTopics from './FeatureSingleTopics/FeatureSingleTopics';
+import React from 'react';
 import { GiReceiveMoney } from 'react-icons/gi';
 import {
     MdInstallMobile, MdPermPhoneMsg, MdCreditCard, MdBolt, MdOutlineReceipt, MdAccountBalance,
@@ -8,26 +7,6 @@ import {
 import './HomeFeatureSection.css';
 
 const HomeFeatureSection = () => {
-    const [sendMoney, setSendMoney] = useState(false);
-    const [mbRecharge, setMbRecharge] = useState(false);
-
-
-    const toggleIsSendMoney = () => {
-        setSendMoney(current => !current);
-    };
-
-    const toggleIsMbRecharge = () => {
-        setMbRecharge(current => !current);
-    };
-
-    function on() {
-        document.getElementById("overlay").style.display = "block";
-    }
-
-    function off() {
-        document.getElementById("overlay").style.display = "none";
-    }
-
     return (
         <div className='feature_bg text-center' id='featureSection'>
             <h2 className='featureHead animate_left'>All the Features to Support <br /> Your Payment Experience</h2>
@@ -36,88 +15,103 @@ const HomeFeatureSection = () => {
             <div className='feature_items col-10 mx-auto animate_bottom'>
 
 
-
-
-                {/* <div className="wrapper">
-                    <div className="bg-image">
-                        <p className="title">Title Data Show</p>
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdInstallMobile className='featureIcon' />
                     </div>
-                </div> */}
-
-
-
-
-                <div onMouseDown={toggleIsSendMoney} className='featureTopicsArea wrapper'>
-                    <MdInstallMobile className='featureIcon' />
                     <p className='featureTopics'>Add Money</p>
-                    <p className="title">Add Money Data Show</p>
+                    <p className="title">Visa Card, Master Card, ATM Card, General Banking, Mobile Banking</p>
                 </div>
 
 
-                <div onClick={toggleIsMbRecharge} className='featureTopicsArea '  >
-                    <MdPermPhoneMsg className='featureIcon' />
+                <div className='featureTopicsArea wrapper'  >
+
+                    <div className=' me-5'><MdPermPhoneMsg className='featureIcon' /></div>
                     <p className='featureTopics'>Mobile Recharge</p>
-                    {mbRecharge &&
-                        <div className=' dataList'>
-                            <p>Mobile Recharge</p>
-                            <p>Another action</p>
-                            <p>Something else here</p>
-                        </div>
-                    }
-
-
+                    <p className="title">Prepaid, postpaid Mobile Recharge</p>
                 </div>
 
-                <div className='featureTopicsArea'>
-                    <MdCreditCard className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdCreditCard className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Travel Card</p>
+                    <p className="title">International Travel Card for USD, Local travel card for BDT</p>
                 </div>
 
 
-                <div className='featureTopicsArea'>
-                    <MdBolt className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdBolt className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Fuel Pay</p>
+                    <p className="title">refuel payments and electric Vichel charging payments</p>
                 </div>
 
 
-                <div className='featureTopicsArea'>
-                    <MdOutlineReceipt className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdOutlineReceipt className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Bill Pay</p>
+                    <p className="title">Utility bill, academic fees, insurance premium, doctors fees, diagnostic bill </p>
                 </div>
 
-                <div className='featureTopicsArea'>
-                    <MdAccountBalance className='featureIcon' />
-                    <p className='featureTopics'>General Transfer</p>
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdAccountBalance className='featureIcon' />
+                    </div>
+                    <p className='featureTopics'>General Banking</p>
+                    <p className="title">Bank Transfer, mobile bank transfer, Credit Card bill</p>
                 </div>
 
-                <div className='featureTopicsArea'>
-                    <GiReceiveMoney className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <GiReceiveMoney className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>E Loan</p>
+                    <p className="title">20000TK instant loan without any document</p>
                 </div>
-                <div className='featureTopicsArea'>
-                    <MdMobileFriendly className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+
+                    <div className=' me-5'>
+                        <MdMobileFriendly className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Mobile Banking</p>
+                    <p className="title">Money transfer and cashout service, bill pay </p>
                 </div>
 
-                <div className='featureTopicsArea'>
-                    <MdOutlineBookOnline className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdOutlineBookOnline className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Ticketing</p>
+                    <p className="title">Bus, train, air ticketing service</p>
                 </div>
 
-                <div className='featureTopicsArea'>
-                    <MdOutlineHowToVote className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdOutlineHowToVote className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Donation</p>
+                    <p className="title"> Donate to Anjuman Mofidul Islam, Quantum Foundation, Red Cresent Society, or any other organization </p>
                 </div>
 
 
-                <div className='featureTopicsArea'>
-                    <MdAssuredWorkload className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdAssuredWorkload className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Govt Fees</p>
+                    <p className="title">VAT,  TAX, AIT, DGDA, BSTI, CCIE, BLPA, or any other govt Fees can be paid</p>
                 </div>
 
-                <div className='featureTopicsArea'>
-                    <MdWallet className='featureIcon' />
+                <div className='featureTopicsArea wrapper'>
+                    <div className=' me-5'>
+                        <MdWallet className='featureIcon' />
+                    </div>
                     <p className='featureTopics'>Salary Pay</p>
+                    <p className="title">Employee's Salary can be paid</p>
                 </div>
             </div>
         </div>
